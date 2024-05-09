22e98d2029c884928028995c873cc3fd56d09e0e
MinoriPerera <minoriperera1212@gmail.com>
2024-05-09 09:44:16 +0530

    Bubble Sort method

---
+++ DataStructuresandAlgorithms/src/datastructuresandalgorithms/BubbleSort.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package datastructuresandalgorithms;
+
+/**
+ *
+ * @author Asus
+ */
+public class BubbleSort {
+    public static void main(String []args){
+        int[] array={12,11,2,18,87,34,27,56,43,33};
+        int i;
+        int j;
+        System.out.println("Before sort");
+        for(i=0; i<array.length-1; i++){
+            System.out.print(array[i]+",");
+        }
+        System.out.println("");
+        for(i=1; i<array.length-1;i++){
+            for(j=0; j<array.length-i;j++){
+                 if(array[j]>array[j+1]){
+                    int temp=array[j];
+                    array[j]=array[j+1];
+                     array[j+1]=temp;
+                 }
+            }
+        }
+        System.out.println("after sort");
+        for(i=0; i<array.length-1; i++){
+            System.out.print(array[i]+",");
+        }
+
+    }
+}
+    
